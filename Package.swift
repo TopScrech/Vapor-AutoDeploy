0aecc8b8a4acb3d7db786c0a563986637c30f5e4
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-12 18:57:48 +0100

    setup

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "mottzi",
+    platforms: [
+        .macOS(.v13),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.29.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                
+            ],
+            swiftSettings: swiftSettings
+        ),
+    ],
+    swiftLanguageModes: [.v5]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
---
+++ Sources/App/app.swift
@@ -0,0 +1,61 @@
+import Vapor
+import Leaf
+import NIOSSL
+
+@main
+struct App
+{
+    static func main() async throws
+    {
+        let env = try Environment.detect()
+        let app = try await Application.make(env)
+
+        app.views.use(.leaf)
+        app.setupRoutes()
+        
+        try await app.execute()
+        try await app.asyncShutdown()
+    }
+}
+
+extension Application
+{
+    func setupRoutes()
+    {
+        self.get("text")
+        { _ in
+            "This is some plain text."
+        }
+
+        self.get("dynamic", ":property")
+        { request async in
+            "Hello, \(request.parameters.get("property")!)!"
+        }
+        
+        self.get("infile")
+        { request async throws in
+            try await request.view.render("htmlFile")
+        }
+        
+        self.get("inline")
+        { _ in
+            let response = Response(status: .ok)
+            response.headers.contentType = .html
+            response.body = .init(stringLiteral:
+            """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>Index Page</title>
+            </head>
+            <body>
+                <h1>inline</h1>
+                <p>This html page is defined in the route definition.</p>
+            </body>
+            </html>
+            """)
+            
+            return response
+        }
+    }
+}
