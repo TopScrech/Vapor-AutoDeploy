84b69269efbf918c94b7905d8f10b58986307ed1
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-15 12:55:13 +0100

    sdf

--- Sources/mottzi/app.swift
+++ Sources/mottzi/app.swift
@@ -31,7 +31,7 @@ extension Application
         self.get("text")
         { _ in
             """
-            Version 1000.0
+            Version 2000.0
             Joshi stinkt.
             """
         }
