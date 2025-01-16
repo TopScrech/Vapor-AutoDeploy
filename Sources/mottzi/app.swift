c24d93f451e1b5a9a27e54e0863c0931f8870a3a
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-16 18:31:05 +0100

    dddd

--- Sources/mottzi/app.swift
+++ Sources/mottzi/app.swift
@@ -34,7 +34,7 @@ extension Application
         // mottzi.de/text
         self.get("text")
         { req throws in
-            throw Abort(.forbidden, reason: "Error: WWWHTTP... ABC... HAHA")
+            throw Abort(.forbidden, reason: "Error: Trump... ABC... HAHA")
             return """
             Version 1
             Joshi stinkt.
