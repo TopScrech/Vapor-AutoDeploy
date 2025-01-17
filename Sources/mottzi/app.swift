23b800d8ba59cce58ee2af778574241a6ff5a511
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-17 15:38:27 +0100

    sdfdsf

--- Sources/mottzi/app.swift
+++ Sources/mottzi/app.swift
@@ -34,7 +34,7 @@ extension Application
         self.get("text")
         { request in
             """
-            FDPasdasd
+            FDPasdasdsdfsdf
             oöp
             """
         }
