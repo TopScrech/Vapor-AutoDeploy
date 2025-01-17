9558bf7f3d69d1d718458e3408aff0f2bd0c76fd
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-17 11:33:00 +0100

    log

--- Sources/mottzi/log.swift
+++ Sources/mottzi/log.swift
@@ -1,19 +1,30 @@
-import Foundation
+import Vapor
 
 // appends content at the end of file
 func log(_ filePath: String, _ content: String)
 {
+    // vapor logger
+    let logger = Logger(label: "mottzi")
+    
     // create log file if it does not exist
     if !FileManager.default.fileExists(atPath: filePath) {
         FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
     }
     
-    // prepare log file
-    guard let file = try? FileHandle(forWritingTo: URL(fileURLWithPath: filePath)) else { return }
-    guard (try? file.seekToEnd()) != nil else { return }
-    guard let data = content.data(using: .utf8) else { return }
-    
-    // append content
-    file.write(data)
-    file.closeFile()
+    guard let data = content.data(using: .utf8) else { return logger.debug("tried logging empty data") }
+
+    do
+    {
+        // go to end of log file
+        let file = try FileHandle(forWritingTo: URL(fileURLWithPath: filePath))
+        try file.seekToEnd()
+        
+        // write content to log file
+        file.write(data)
+        file.closeFile()
+    }
+    catch
+    {
+        logger.error("\(error.localizedDescription)")
+    }
 }
