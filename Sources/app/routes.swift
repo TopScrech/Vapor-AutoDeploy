925a083c57a1fb2cb9a4373c448ba96ee24f4565
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-27 14:26:17 +0100

    3

--- Sources/app/routes.swift
+++ Sources/app/routes.swift
@@ -35,7 +35,7 @@ extension Application
         self.get("text")
         { request in
             """
-            Auto deploy: ? YAS2 ?
+            Auto deploy: ? YAS3 ?
             """
         }
         
