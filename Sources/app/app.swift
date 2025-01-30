cad87cc98b2365dd7d9efe876304c021c8497d30
Berken Sayilir <sayilir.berken@gmail.com>
2025-01-30 01:32:08 +0100

    lol

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -15,15 +15,6 @@ struct mottzi
         app.databases.use(.sqlite(.file("deploy/github/deployments.db")), as: .sqlite)
         app.migrations.add(Deployment.Table2())
         try await app.autoMigrate()
-
-        let corsConfiguration = CORSMiddleware.Configuration(
-            allowedOrigin: .all,
-            allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-            allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
-        )
-        let cors = CORSMiddleware(configuration: corsConfiguration)
-        // cors middleware should come before default error middleware using `at: .beginning`
-        app.middleware.use(cors, at: .beginning)
         
         app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
         app.views.use(.leaf)
