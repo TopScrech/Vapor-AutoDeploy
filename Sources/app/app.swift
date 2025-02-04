585e2f2c4a270f7ce79c0996b7367a509b654233
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-04 12:24:30 +0100

    env refactor

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -28,27 +28,3 @@ struct mottzi
         try await app.asyncShutdown()
     }
 }
-
-extension Environment
-{
-    enum Variables: String, CaseIterable
-    {
-        case GITHUB_WEBHOOK_SECRET
-        
-        var value: String
-        {
-            Environment.get(self.rawValue)!
-        }
-    }
-    
-    func useVariables()
-    {
-        for variable in Variables.allCases
-        {
-            if Environment.get(variable.rawValue) == nil
-            {
-                fatalError("\(variable.rawValue): Environment variable not found.")
-            }
-        }
-    }
-}
---
+++ Sources/app/env.swift
@@ -0,0 +1,25 @@
+import Vapor
+
+extension Environment
+{
+    enum Variables: String, CaseIterable
+    {
+        case GITHUB_WEBHOOK_SECRET
+        
+        var value: String
+        {
+            Environment.get(self.rawValue)!
+        }
+    }
+    
+    func useVariables()
+    {
+        for variable in Variables.allCases
+        {
+            if Environment.get(variable.rawValue) == nil
+            {
+                fatalError("\(variable.rawValue): Environment variable not found.")
+            }
+        }
+    }
+}
