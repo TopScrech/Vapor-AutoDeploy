ac5a69a60d619ee37ceea93fe536373e32b8a60b
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-04 15:14:02 +0100

    ssss

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -9,10 +9,16 @@ struct mottzi
     static func main() async throws
     {
         var env = try Environment.detect()
-        try LoggingSystem.bootstrap(from: &env)
+        //try LoggingSystem.bootstrap(from: &env)
+        LoggingSystem.bootstrap(fragment: customLoggerFragment(), console: Terminal(), level: try Logger.Level.detect(from: &env))
+        func customLoggerFragment() -> some LoggerFragment {
+            LabelFragment().maxLevel(.trace)
+                .and(LevelFragment().separated(" ").and(MessageFragment().separated(" ")))
+                .and(MetadataFragment().separated(" "))
+                .and(SourceLocationFragment().separated(" ").maxLevel(.info))
+        }
         
         let app = try await Application.make(env)
-        app.logger.logLevel = .info
         app.environment.useVariables()
         app.databases.use(.sqlite(.file("deploy/github/deployments.db")), as: .sqlite)
         app.databases.middleware.use(DeploymentListener(), on: .sqlite)
