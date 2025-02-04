cedd5f811626940449f8c568e6f06d1fe213d66f
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-04 15:51:32 +0100

    asdsdfsdfsd

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -9,16 +9,30 @@ struct mottzi
     static func main() async throws
     {
         var env = try Environment.detect()
-        //try LoggingSystem.bootstrap(from: &env)
-        LoggingSystem.bootstrap(fragment: customLoggerFragment(), console: Terminal(), level: try Logger.Level.detect(from: &env))
-        func customLoggerFragment() -> some LoggerFragment {
-            LabelFragment().maxLevel(.trace)
-                .and(LevelFragment().separated(" ").and(MessageFragment().separated(" ")))
-                .and(MetadataFragment().separated(" "))
-                .and(SourceLocationFragment().separated(" ").maxLevel(.info))
-        }
+        
+//        let fragment = LabelFragment().maxLevel(.trace)
+//            .and(LevelFragment().separated(" ")
+//            .and(MessageFragment().separated(" ")))
+//            .and(MetadataFragment().separated(" "))
+//            .and(SourceLocationFragment().separated(" ").maxLevel(.info))
+//        
+//        LoggingSystem.bootstrap(fragment: fragment, console: Terminal(), level: try Logger.Level.detect(from: &env))
         
         let app = try await Application.make(env)
+        
+        try LoggingSystem.bootstrap(from: &env)
+        
+        let fragment = LabelFragment().maxLevel(.trace)
+            .and(LevelFragment().separated(" ")
+            .and(MessageFragment().separated(" ")))
+            .and(MetadataFragment().separated(" "))
+            .and(SourceLocationFragment().separated(" ").maxLevel(.info))
+        
+        app.logger = .init(label: "app")
+        {
+            ConsoleFragmentLogger(fragment: fragment, label: $0, console: Terminal())
+        }
+        
         app.environment.useVariables()
         app.databases.use(.sqlite(.file("deploy/github/deployments.db")), as: .sqlite)
         app.databases.middleware.use(DeploymentListener(), on: .sqlite)
