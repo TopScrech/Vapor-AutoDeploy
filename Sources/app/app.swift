fcc8975fc6868088f5cd8d95f6d652d6d31c6118
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-04 16:05:17 +0100

    finalk

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -9,32 +9,21 @@ struct mottzi
     static func main() async throws
     {
         var env = try Environment.detect()
-        
-//        let fragment = LabelFragment().maxLevel(.trace)  
-//            .and(LevelFragment().separated(" ")
-//            .and(MessageFragment().separated(" ")))
-//            .and(MetadataFragment().separated(" "))
-//            .and(SourceLocationFragment().separated(" ").maxLevel(.info))
-//        
-//        LoggingSystem.bootstrap(fragment: fragment, console: Terminal(), level: try Logger.Level.detect(from: &env))
-        
+        try LoggingSystem.bootstrap(from: &env)
+
         let app = try await Application.make(env)
         
-        try LoggingSystem.bootstrap(from: &env)
-        
-        let fragment = LabelFragment().maxLevel(.trace)
-            .and(LevelFragment().separated(" ")
-            .and(MessageFragment().separated(" ")))
-            .and(MetadataFragment().separated(" "))
-            .and(SourceLocationFragment().separated(" ").maxLevel(.info))
-        
-        app.logger = Logger(label: "app")
+        app.logger = Logger(label: "mottzi")
         {
-            ConsoleFragmentLogger(fragment: fragment, label: $0, console: Terminal())
+            let format = LabelFragment().maxLevel(.trace)
+                .and(LevelFragment().separated(" ")
+                .and(MessageFragment().separated(" ")))
+                .and(MetadataFragment().separated(" "))
+                .and(SourceLocationFragment().separated(" ").maxLevel(.info))
+            
+            return ConsoleFragmentLogger(fragment: format, label: $0, console: Terminal(), level: .info)
         }
-        
-        app.logger.logLevel = .info
-        
+                
         app.environment.useVariables()
         app.databases.use(.sqlite(.file("deploy/github/deployments.db")), as: .sqlite)
         app.databases.middleware.use(DeploymentListener(), on: .sqlite)
