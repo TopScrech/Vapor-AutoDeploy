122380d3185a7fbd90de0ea19ba08aad782dc270
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-05 23:16:22 +0100

    lol

--- Sources/app/routes/deployment.swift
+++ Sources/app/routes/deployment.swift
@@ -4,7 +4,7 @@ extension Application
 {
     func useDeployPanel()
     {
-        self.webSocket("deploy", "ws")
+        self.webSocket("panel", "ws")
         { request, ws async in
             // make client connection identifiable
             let id = UUID()
@@ -21,7 +21,7 @@ extension Application
             ws.onClose.whenComplete() { _ in Task { await DeploymentClients.shared.remove(connection: id) } }
         }
         
-        self.get("deploy")
+        self.get("panel")
         { request async throws -> View in
             let deployments = try await Deployment.all(on: request.db)
             
