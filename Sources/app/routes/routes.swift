1ac4ee2f0e506ff95d4bacaaf9301a2675b5cea9
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-06 13:30:00 +0100

    7

--- Sources/app/routes/routes.swift
+++ Sources/app/routes/routes.swift
@@ -6,7 +6,7 @@ extension Application
     public func useRoutes()
     {
         // mottzi.de/route
-        self.get("route") { _ in "6" }
+        self.get("route") { _ in "7" }
         
         // mottzi.de/template
         self.get("template")
