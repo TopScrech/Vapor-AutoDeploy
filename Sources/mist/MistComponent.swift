867a44d37ea487250fd7e03c2b0f31afa8b8ff03
Berken Sayilir <sayilir.berken@gmail.com>
2025-02-14 21:07:54 +0100

    gggg

--- Sources/mist/MistComponent.swift
+++ Sources/mist/MistComponent.swift
@@ -1,25 +1,48 @@
 import Vapor
 import Fluent
 
+// Type-erasing wrapper for Encodable values
+struct AnyEncodable: Encodable {
+    private let _encode: (Encoder) throws -> Void
+    
+    init(_ value: any Encodable) {
+        self._encode = { encoder in
+            try value.encode(to: encoder)
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        try _encode(encoder)
+    }
+}
+
+// Context structure that can handle type-erased Encodable values
+struct MistContext: Encodable {
+    let entry: AnyEncodable
+    
+    init(entry: any Encodable) {
+        self.entry = AnyEncodable(entry)
+    }
+}
+
 // Basic component protocol - just needs to know its model type
 protocol MistComponent
 {
-    associatedtype ModelType: Encodable
-    
     static var name: String { get }
     static var model: String { get }
     static var template: String { get }
     
-    static func html(renderer: ViewRenderer, model: ModelType) async -> String?
+    static func html(renderer: ViewRenderer, model: any Encodable) async -> String?
 }
 
 extension MistComponent
 {
-    static func html(renderer: ViewRenderer, model: ModelType) async -> String?
+    static func html(renderer: ViewRenderer, model: any Encodable) async -> String?
     {
         do
         {
-            let buffer = try await renderer.render(self.template, ["entry": model]).data
+            let context = MistContext(entry: model)
+            let buffer = try await renderer.render(self.template, context).data
             return String(buffer: buffer)
         }
         catch
@@ -57,8 +80,6 @@ actor MistComponentRegistry
 
 struct DummyRowComponent: MistComponent
 {
-    typealias ModelType = DummyModel
-    
     static let name = "DummyRow"
     static let model = "DummyModel"
     static let template = "DummyRow"
