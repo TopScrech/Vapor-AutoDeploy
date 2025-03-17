8165dca1d758d02eaf8276df8cbe2f2201e7a1d0
Berken Sayilir <sayilir.berken@gmail.com>
2025-03-17 17:10:05 +0100

    failtest

--- Package.swift
+++ Package.swift
@@ -8,20 +8,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mottzi/Mist", from: "0.1.0"),
-//        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
-//        .package(url: "https://github.com/vapor/fluent.git", from: "4.12.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.8.0"),
-//        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Mist", package: "Mist"),
-//                .product(name: "Vapor", package: "vapor"),
-//                .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-//                .product(name: "Leaf", package: "leaf"),
             ],
             swiftSettings: swiftSettings
         ),
--- Sources/mist/Example/DummyRow.swift
+++ Sources/mist/Example/DummyRow.swift
@@ -7,4 +7,14 @@ struct DummyRow: Mist.Component
 {
     // implicit 1-to-1 relantionship of all models through common id
     static let models: [any Mist.Model.Type] = [DummyModel1.self, DummyModel2.self]
+    
+    public static func makeContext(of componentID: UUID, in db: Database) async -> Mist.SingleComponentContext?
+    {
+        return nil
+    }
+    
+    public static func makeContext(ofAll db: Database) async -> Mist.MultipleComponentContext?
+    {
+        return nil
+    }
 }
